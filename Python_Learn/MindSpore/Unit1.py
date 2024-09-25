075893381cc1d52246e94a8665d90f74122c3c29
Pan BingHong <19909442097@163.com>
2024-09-25 21:52:57 +0800

    演示如何提交到个人Github仓库

---
+++ Python_Learn/MindSpore/Unit1.py
@@ -0,0 +1,15 @@
+def main():
+    print('主函数入口')
+
+if __name__ == '__main__':
+    main()
+
+"""
+author: xxx
+date: xxxx
+"""
+'''
+git config --global user.email "19909442097@163.com"
+git config --global user.name "Pan BingHong"
+'''
+
